ed45c0d0303479ad1ed8a41513c1aeee15b5bdd3
kwwk <kwwk747@gmail.com>
2024-05-23 23:30:42 +0200

    todo

--- pill_app/main.cpp
+++ pill_app/main.cpp
@@ -8,3 +8,10 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+
+
+//TODO
+//zapisywanie do pliku aktualnie branych lekow na podstawie wczesniej dodanych
+//zczytanie danych z pliku i pokazywanie na biezaco w kalendarzu dni w ktorych wzieto dane leki
+//mozliwosc usuniecia danego leku
+//quantity - pokazywanie aktualnego stanu dostepnosci, jak wprowadziles 30, a pozniej zaznaczyles ze wziales to bedzie 29 itp
